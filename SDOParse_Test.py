8f30c2ba5c36cea82adc591db005d55790bad002
MilesGordon <miles@miredmusic.com>
2017-12-11 13:39:13 -0500

    testing

--- SDOParse_Test.py
+++ SDOParse_Test.py
@@ -23,5 +23,7 @@ def Parse_Directory(WLEN):
 
 new_index = Parse_Directory(wavelength)
 
-hdulist = fits.open(new_index[1])
+print("IS IT A PATH? " + str(os.path.exists(new_index[1])))
+
+hdulist = fits.open(str(new_index[1]))
 
