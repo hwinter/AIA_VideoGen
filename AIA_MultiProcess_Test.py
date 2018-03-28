782daa4d2621af8a5bc44397acbaba28e8211559
MilesGordon <miles@miredmusic.com>
2018-03-28 15:07:22 -0400

    it never works the first time

--- AIA_MultiProcess_Test.py
+++ AIA_MultiProcess_Test.py
@@ -26,6 +26,7 @@ import sys
 timestart = datetime.datetime.now()
 
 fontpath = os.path.dirname(os.path.abspath("BebasNeue Regular.otf"))
+print(fontpath)
 font = ImageFont.truetype(fontpath, 19)
 
 database = []
