9116ec3c0c550255761870acd4a2d796eab07111
Miles <miles@miredmusic.com>
2018-06-15 15:24:14 -0400

    Cleanup + Adding Logos

--- FROST_Production.py
+++ FROST_Production.py
@@ -49,7 +49,7 @@ if len(sys.argv) == 3:
 else:
 	print("This script takes exactly two arguments. Proceeding with default values. ")
 	directory = "/data/SDO/AIA/synoptic/" + str(year) + "/" + str(month) +"/" + str(day) + "/"
-	skipframes = 1
+	skipframes = 2
 
 print("Dataset: " + str(directory))
 
